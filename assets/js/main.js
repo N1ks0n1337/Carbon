48e88df0a3a62bfb30d4f71dffac5f51b5c38cba
N1ks0n1337 <92136787+N1ks0n1337@users.noreply.github.com>
2022-05-27 11:22:13 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
- /*-------LOGIN.VAL-----*/
+ /*-------Validation-----*/
  let form = document.querySelector('.js-form'),
      formInputs = document.querySelectorAll('.js-input'),
      inputEmail = document.querySelector('.js-input-email'),
      inputPassword = document.querySelector('.js-input-password');
-     formBtn = document.querySelector('.form__btn');
-     
+     formBtn = document.querySelector('.form__btn'),
+     errorElement = document.getElementById('error'),
+     addInputPopup = document.querySelectorAll('.js-input-popup-add'),
+     formBtnAdd = document.querySelector('.form__btn-add'),
+     operatorsBtn = document.querySelectorAll('.operatorsBtn'),
+     formBtnOperators = document.querySelector('.form__btn-operators');
+
 
 
  function validateEmail(email) {
@@ -12,10 +17,24 @@
      return re.test(String(email).toLowerCase());
  }
 
- form.onsubmit = function () {
+ form.onsubmit = function (e) {
      let emailVal = inputEmail.value,
          passVal = inputPassword.value;
      emptyInputs = Array.from(formInputs).filter(input => input.value === '')
+
+     let messages = []
+     if (inputEmail.value === '' || inputEmail == null) {
+         messages.push('Email entered incorrectly')
+     }
+
+     if(passVal.length <= 6){
+        messages.push('Password must be longer that 6 characters')
+    }
+
+     if (messages.length > 0) {
+         e.preventDefault()
+         errorElement.innerText = messages.join('. ')
+     }
 
      formInputs.forEach(function (input) {
          if (input.value === '') {
@@ -26,13 +45,9 @@
          }
      })
 
-
-
      if (emptyInputs.length !== 0) {
-        
          return false
      }
-
      if (!validateEmail(emailVal)) {
          inputEmail.classList.add('error');
          return false;
@@ -40,16 +55,42 @@
          inputEmail.classList.remove('error');
      }
  }
+
+
  function test() {
-    formInputs.forEach(item => {
-        if (item.value.length != 0) {
-            if (!item.classList.contains('active-btn')) {
-                formBtn.classList.add('active-btn')
-            }
-        } else {
-            formBtn.classList.remove('active-btn')
-        }
-    })
-}
+     formInputs.forEach(item => {
+         if (item.value.length != 0) {
+             if (!item.classList.contains('active-btn')) {
+                 formBtn.classList.add('active-btn')
+             }
+         } else {
+             formBtn.classList.remove('active-btn')
+         }
+     })
+ }
+
+ function add() {
+     addInputPopup.forEach(item => {
+         if (item.value.length != 0) {
+             if (!item.classList.contains('active-btn')) {
+                 formBtnAdd.classList.add('active-btn')
+             }
+         } else {
+             formBtnAdd.classList.remove('active-btn')
+         }
+     })
+ }
+
+ function operators() {
+     operatorsBtn.forEach(item => {
+         if (item.value.length != 0) {
+             if (!item.classList.contains('active-btn')) {
+                 formBtnOperators.classList.add('active-btn')
+             }
+         } else {
+             formBtnOperators.classList.remove('active-btn')
+         }
+     })
+ }
 
  /*--------------------*/
